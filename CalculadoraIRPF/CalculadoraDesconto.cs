897dfb91d825211a8556f2fc64a7db0656685c7e
lucas.rodrigues <lucas.rodrigues@placeti.com.br>
2025-12-31 13:42:51 -0300

    refactor: utiliza s do solid e modifica convençao de case para pascal case

---
+++ CalculadoraIRPF/CalculadoraDesconto.cs
@@ -0,0 +1,12 @@
+namespace CalculadoraIRPF;
+
+public class CalculadoraDesconto {
+    public static double Calcular(double salarioBruto) {
+        return salarioBruto switch {
+            <= 2000 => 0,
+            <= 3000 => salarioBruto * 0.08,
+            <= 4500 => salarioBruto * 0.18,
+            _ => salarioBruto * 0.28
+        };
+    }
+}
